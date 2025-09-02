633a4a5018e2025abaf37a3f3cda3eafd2adfcd9
arrizarisky <mhdarriza1404@gmail.com>
2025-09-02 08:10:44 +0700

    Refactor App component structure and fix JSX syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { Home } from "./pages/Home";
 
 function App() {
-
   return (
-    <>
+    <div className="px-4 sm:px-6 lg:px-8">
       <Home />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
